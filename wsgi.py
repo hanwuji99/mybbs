f95d6c0babadba655f7497b687198d7a458c8830
hanwuji99 <hanwuji99@outlook.com>
2017-05-02 19:28:57 +0800

    部署修改

--- wsgi.py
+++ wsgi.py
@@ -11,17 +11,17 @@ application = app.app
 
 """
 建立一个软连接
-ln -s /var/www/bbs/bbs.conf /etc/supervisor/conf.d/bbs.conf
+ln -s /var/www/mybbs/bbs.conf /etc/supervisor/conf.d/bbs.conf
 
-ln -s /var/www/bbs/bbs.nginx /etc/nginx/sites-enabled/bbs
+ln -s /var/www/mybbs/bbs.nginx /etc/nginx/sites-enabled/bbs
 
 
 
 ➜  ~ cat /etc/supervisor/conf.d/bbs.conf
 
-[program:bbs]
+[program:mybbs]
 command=/usr/local/bin/gunicorn wsgi -c gunicorn.config.py
-directory=/var/www/bbs
+directory=/var/www/mybbs
 autostart=true
 autorestart=true
 
