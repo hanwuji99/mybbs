458de4de2c0c96bc8f788aa137ba9ec0f03e0c1d
hanwuji99 <hanwuji99@outlook.com>
2017-05-02 19:31:51 +0800

    上传头像路径修改

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 secret_key = 'web18 bbs'
-user_file_director = r'C:\Users\wxlay\Desktop\bbs\user_image'
+user_file_director = r'/var/www/mybbs/user_image'
 accept_user_file_type = ['jpg', 'gif', 'png']
